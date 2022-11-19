b9f28c0070ff0a0727da1a8f223feabb5f16fcfc
Brent Lombaard <lombaardbrent@gmail.com>
2022-11-19 12:39:03 +0200

    Vid 27 Connection String

---
+++ DailyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DailyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
