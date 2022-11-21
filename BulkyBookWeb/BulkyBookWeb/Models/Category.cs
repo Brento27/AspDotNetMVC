e44a0fba226d54e549d608211a0ae9700a3be7b0
Brent Lombaard <lombaardbrent@gmail.com>
2022-11-21 08:53:53 +0200

    Vid 22 Create Category model

---
+++ BulkyBookWeb/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
